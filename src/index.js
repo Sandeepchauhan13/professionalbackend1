0b6b9545ac43013546124afc93babd4a9fde08be
apnainstitute-demo <sinh.pozta1@gmail.com>
2024-01-17 13:06:23 +0530

    database connection used try catch env etc

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// #note: as early as possible  in your application, import and configure dotenv: 
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+// import express from 'express';
+
+// const app = express ()
+// async await is used if database is located in another continent 
+// ( async () =>{
+//     try{
+//     await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     app.on("error", ()=>{
+//         console.log("ERR: ", error);
+//         throw error
+//     })
+//     app.listen(process.env.PORT, ()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//     })
+//     }catch (error){
+//     console.error("ERROR: ",  error)
+//     throw err
+//     }
+// }) ()
